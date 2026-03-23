dfce413cfd419b769aecd659527f77387c0a4613
Valerie Hirsch <Valerie.Hirsch@stud.hcw.ac.at>
2026-03-23 15:11:57 +0100

    part 1 done

--- server/server.js
+++ server/server.js
@@ -5,10 +5,41 @@ const app = express()
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
 
+
+
 // Configure a 'get' endpoint for data..
 app.get('/movies', function (req, res) {
-  // Part 1: Remove the next line and replace with your code
-  res.send('!dlrow olleH')
+    fetch("http://www.omdbapi.com/?apikey=a8cbe32d&s=Batman")
+      .then(response => response.json())
+      .then(data => {
+      const threeMovies = data.Search.slice(0,3)
+      
+      Promise.all(
+       threeMovies.map(movie => 
+        fetch(`http://www.omdbapi.com/?apikey=a8cbe32d&i=${movie.imdbID}`)
+        .then(response2 => response2.json())
+      )
+    ).then(fullMovies => {
+
+        const structured = fullMovies.map(movie => ({
+          Title: movie.Title,
+          Released: new Date(movie.Released).toLocaleDateString('en-CA'),
+          Runtime: parseInt(movie.Runtime),
+          Genres: (movie.Genre).split(','),
+          Directors: (movie.Director).split(','),
+          Writers: (movie.Writer).split(','),
+          Actors: (movie.Actors).split(','),
+          Plot: movie.Plot,
+          Poster: movie.Poster,
+          Metascore: parseInt(movie.Metascore),
+          imdbRating: parseFloat(movie.imdbRating)
+        }))
+        res.json(structured)
+      })
+
+      
+    })
+  
 })
 
 app.listen(3000)
